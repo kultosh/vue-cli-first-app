66a8528832ebd7b2069c256ebd09ce56e78112ec
kultosh <kulgole90@gmail.com>
2020-05-10 17:28:05 +0545

    This is the first vue-cli

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>{{title}}</h1>
+    <p>
+      {{greeting()}}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return{
+        title: 'Vue-CLI',
+    }
+  },
+  methods: {
+    greeting:function(){
+      return "Namaste";
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
