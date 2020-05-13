35e4ac08c17ed9bf6c9105b05ee25a1742f6ca16
kultosh <kulgole90@gmail.com>
2020-05-13 15:44:47 +0545

    Created input binding using v-model as in vue-first-step lesson to create a simple preview of the blog & some of the extra features are lazy and also created the properties for same types like blog property for title and content

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,26 @@
 <template>
   <div>
-    <h1>{{title}}:</h1>
-    <hero></hero>
+    <add-blog></add-blog>
   </div>
 </template>
 
 <script>
+import addBlog from './components/addBlog.vue';
 export default {
+  components: {
+    'add-blog': addBlog
+  },
   data () {
     return{
-        title: 'Nesting Compnent Globally',
+
     }
   }
 }
 </script>
 
 <style>
-
+  body{
+    margin: 0;
+    font-family: 'Nunito SemiBold';
+  }
 </style>
